4e498661a826d440686f8b505074a381417e6e3c
Facundo Moreno <zedshenkassa@gmail.com>
2023-02-03 22:05:28 -0300

    Second commit

---
+++ front/src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import style from "./About.module.css";
+
+const About = () => {
+    return(
+        <div className={style.container} >
+            <h1>Creador: Facundo Moreno</h1>
+            <h2>Solo soy un simple estudiante intentando hacer una página</h2>
+        </div>
+    )
+}
+
+export default About;
